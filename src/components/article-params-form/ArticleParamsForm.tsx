48abfd400399e4e2b061a7cca6bd04b2357c7d23
Polina Slonina <sloninapp@mail.ru>
2025-03-22 09:12:56 +0300

    реализация работы формы и применения настроек сайдбара

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,136 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useEffect, useRef, useState } from 'react';
+import clsx from 'clsx';
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	OptionType,
+} from 'src/constants/articleProps';
+import { Text } from 'src/ui/text';
+import { Select } from 'src/ui/select';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsProps = {
+	setArticleState: (ArticleState: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({ setArticleState }: ArticleParamsProps) => {
+	const [isOpen, setIsOpen] = useState(false); // Начальное значение false означает, что форма закрыта
+	const sidebarRef = useRef<HTMLDivElement | null>(null); //  для закрытия сайдбара при клике вне его
+
+	const [formState, setFormState] = useState(defaultArticleState);
+
+	// Подписываемся на событие клика документа
+	useEffect(() => {
+		// Функция для закрытия сайдбара при клике вне его
+		const handleSidebarIfClickedOutside = (e: MouseEvent) => {
+			if (
+				sidebarRef.current &&
+				!sidebarRef.current.contains(e.target as Node)
+			) {
+				setIsOpen(false);
+			}
+		};
+
+		if (isOpen && sidebarRef.current) {
+			document.addEventListener('mousedown', handleSidebarIfClickedOutside);
+		}
+
+		// Отписываемся от события при размонтировании компонента
+		return () => {
+			if (sidebarRef.current) {
+				document.removeEventListener(
+					'mousedown',
+					handleSidebarIfClickedOutside
+				);
+			}
+		};
+	}, [isOpen]);
+
+	const handleChangeOption = (
+		field: keyof ArticleStateType,
+		option: OptionType
+	) => {
+		setFormState((prevState) => ({
+			...prevState,
+			[field]: option,
+		}));
+	};
+
+	const handleReset = () => {
+		setFormState(defaultArticleState);
+		setArticleState(defaultArticleState);
+	};
+
 	return (
-		<div>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+		<div ref={sidebarRef}>
+			<ArrowButton isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
+				<form
+					className={styles.form}
+					onSubmit={(e: React.FormEvent<HTMLFormElement>) =>
+						e.preventDefault()
+					}>
+					<Text as={'h2'} size={31} weight={800} uppercase={true}>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(option) =>
+							handleChangeOption('fontFamilyOption', option)
+						}
+						title='Шрифт'
+					/>
+					<RadioGroup
+						name='radio'
+						options={fontSizeOptions}
+						selected={formState.fontSizeOption}
+						onChange={(option) => handleChangeOption('fontSizeOption', option)}
+						title='Размер шрифта'
+					/>
+					<Select
+						selected={formState.fontColor}
+						options={fontColors}
+						onChange={(option) => handleChangeOption('fontColor', option)}
+						title='Цвет шрифта'
+					/>
+					<Separator />
+					<Select
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						onChange={(option) => handleChangeOption('backgroundColor', option)}
+						title='Цвет фона'
+					/>
+					<Select
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						onChange={(option) => handleChangeOption('contentWidth', option)}
+						title='Ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={handleReset}
+						/>
+						<Button
+							title='Применить'
+							htmlType='submit'
+							type='apply'
+							onClick={() => setArticleState(formState)}
+						/>
 					</div>
 				</form>
 			</aside>
